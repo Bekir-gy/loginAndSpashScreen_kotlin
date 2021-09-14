e457cbf652f9f7f69882a9682d57e16f7f7d9dcd
Bekir-gy <bekiryegin10@gmail.com>
2021-09-14 17:06:50 +0300

    upload

---
+++ app/src/main/java/com/bekirgy/bekirprojemobil/DashboardActivity.kt
@@ -0,0 +1,11 @@
+package com.bekirgy.bekirprojemobil
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DashboardActivity: AppCompatActivity(){
+    override fun onCreate(savedInstantState: Bundle?){
+        super.onCreate(savedInstantState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
